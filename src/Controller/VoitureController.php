218e01a5cf185fdc39c55d4f812a252837e1663f
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-12 15:54:14 +0200

    conditions

--- src/Controller/VoitureController.php
+++ src/Controller/VoitureController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use App\DataFixtures\VoitureFixture;
 use App\Repository\VoitureRepository;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Url;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -40,27 +42,22 @@ class VoitureController extends AbstractController
     /**
      * @Route("/voiture/new/", name="newVoiture")
      */
-    public function new(Request $laRequete) : Response
+    public function new(Request $laRequete, EntityManagerInterface $manager) : Response
     {
-        $manager =$this->getDoctrine()->getManager();
-
-        if ($laRequete->request->count() < 0) {
+        if ($laRequete->request->count() > 0) {
             $newVoiture = new Voiture();
             $newVoiture->setName($laRequete->request->get('name'));
             $newVoiture->setBrand($laRequete->request->get('brand'));
             $newVoiture->setPrice($laRequete->request->get('price'));
             $newVoiture->setCreatedAt(new \DateTime());
-
-            dump($newVoiture);
-
             $manager->persist($newVoiture);
             $manager->flush();
+
+            return $this->redirect('http://localhost:8000/voiture');
+        } else {
+            return $this->render('voiture/new.html.twig');
         }
-
-
-        return $this->render('voiture/new.html.twig');
     }
-
 }
 
 
