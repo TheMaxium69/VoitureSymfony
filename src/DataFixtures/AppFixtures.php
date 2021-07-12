7b742496cac9ba926b4199ad502415fc98c14aae
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-07-12 10:40:28 +0200

    add fitxure

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,13 +4,20 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Voiture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $date = new \DateTime('@'.strtotime('now'));
+
+        $voiture = new Voiture();
+        $voiture->setName('test');
+        $voiture->setBrand('test');
+        $voiture->setPrice(1000);
+        $voiture->setCreatedAt(new \DateTime());
+        $manager->persist($voiture);
 
         $manager->flush();
     }
